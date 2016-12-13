8ac2642d2d50da0bfa4e2eff422a6c1ac31595a9
Cyndi Rader <janaidu02@gmial.com>
2016-12-13 22:17:11 +0000

    first commit point

--- src/main/scala/Shopping/Main.scala
+++ src/main/scala/Shopping/Main.scala
@@ -1,7 +1,7 @@
 package Shopping
 
 object Main {
-
+//this is the main program for the shopping cart
   def main(args: Array[String]) {
     println("[ " + args.mkString(", ") + " ] => £" + ShoppingCart.checkoutWithOffers(args))
   }
